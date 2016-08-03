b5a11dbcbd2fed98283efdec9e3d9f2611225bfc
populary <410445919@qq.com>
2016-08-03 09:51:56 +0800

    Initial Commit

---
+++ YX_TableViewToPick/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YX_TableViewToPick
+//
+//  Created by yang on 16/8/3.
+//  Copyright © 2016年 poplary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
